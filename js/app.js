e87cb1e5ef75fe86bf48e171e791cb8db80c89d5
sergio <scabrera.main@gmail.com>
2024-03-25 19:21:30 +0100

    commit15

--- js/app.js
+++ js/app.js
@@ -80,3 +80,27 @@ $.getJSON('/lang/lang.json', function(translations) {
   });
   
 
+    document.addEventListener("DOMContentLoaded", function() {
+
+        const menuLinks = document.querySelectorAll(".ul .a");
+
+
+        menuLinks.forEach(link => {
+
+            link.addEventListener("click", function(event) {
+
+                event.preventDefault();
+
+
+                const targetId = link.getAttribute("href").substring(1);
+
+
+                const targetSection = document.getElementById(targetId);
+
+
+                targetSection.scrollIntoView({ behavior: "smooth" });
+            });
+        });
+    });
+
+
